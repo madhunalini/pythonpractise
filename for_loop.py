7f318ebb3bf4139ff333f3f6cab9b73f3d141788
madhunalini <151796333+madhunalini@users.noreply.github.com>
2023-11-26 17:02:30 +0000

    ok

---
+++ for_loop.py
@@ -0,0 +1,8 @@
+list1= (1,12,33,44,55,999,8588585)
+for x in list1:
+    print(x)
+list1.append(333)
+
+
+
+    
---
+++ list.py
@@ -0,0 +1,9 @@
+list1= ["madhu","Nalini","Aadhya","Shanyu"]
+list1.append("Rama")
+print(list1)
+print(list1[1:3])
+print(list1[0:3])
+print(list1[])
+
+
+
