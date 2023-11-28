30e8fd73a6b802d06383c2ad7a75637a610f12fe
madhunalini <151796333+madhunalini@users.noreply.github.com>
2023-11-28 13:37:06 +0000

    ok

--- fileoperations.py
+++ fileoperations.py
@@ -2,7 +2,7 @@ def file_operations(file_path,key,value):
     with open(file_path,"r") as file:
         lines= file.readlines()
 
-    with open(file_path, "w"):
+    with open(file_path, "w") as file:
         for line in lines:
             if key in line:
                 file.write(key +"="+ value)
