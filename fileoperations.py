425b57b58f9c45225231d07f97c781b8a8fc7444
madhunalini <151796333+madhunalini@users.noreply.github.com>
2023-11-28 13:13:12 +0000

    ok

---
+++ fileoperations.py
@@ -0,0 +1,16 @@
+max_connections=200
+def file_operations(filepath,key, value):
+    with open(filepath, "r") as file:
+        files= file.readline()
+
+    with open(filepath,"w") as file:
+        for line in files:
+            if key in line:
+                file.write(key + "" + value)
+            else:
+                file.write(line)
+
+file_operations(,max_connections,1000)
+
+             
+
